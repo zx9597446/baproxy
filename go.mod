810e32a19f594d0722dbc96c059495d8b4d0d8b0
zx9597446 <zx9597446@gmail.com>
2023-09-21 11:45:27 +0800

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/zx9597446/baproxy
+
+go 1.21.1
+
+require (
+	github.com/kataras/basicauth v0.0.2 // indirect
+	golang.org/x/crypto v0.0.0-20220919173607-35f4265a4bc0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"crypto/rand"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/kataras/basicauth"
+)
+
+var (
+	flgUser, flgPass, flgRemote, flgListen string
+)
+
+func initFlags() {
+	flag.StringVar(&flgUser, "u", "", "username")
+	flag.StringVar(&flgPass, "p", "", "password")
+	flag.StringVar(&flgRemote, "r", "", "remote addr")
+	flag.StringVar(&flgListen, "l", ":8089", "listen addr")
+	flag.Parse()
+}
+
+func main() {
+	initFlags()
+
+	if flgRemote == "" {
+		log.Fatal("remote addr is required")
+	}
+	if flgUser == "" {
+		flgUser = shortID(6)
+	}
+	if flgPass == "" {
+		flgPass = shortID(12)
+	}
+
+	auth := basicauth.Default(map[string]string{
+		flgUser: flgPass,
+	})
+
+	// initialize a reverse proxy and pass the actual backend server url here
+	proxy, err := NewProxy(flgRemote)
+	if err != nil {
+		panic(err)
+	}
+
+	h := ProxyRequestHandler(proxy)
+	newh := basicauth.HandlerFunc(auth, h)
+
+	// handle all requests to your server using the proxy
+	http.HandleFunc("/", newh)
+	log.Fatal(http.ListenAndServe(flgListen, nil))
+}
+
+// NewProxy takes target host and creates a reverse proxy
+func NewProxy(targetHost string) (*httputil.ReverseProxy, error) {
+	url, err := url.Parse(targetHost)
+	if err != nil {
+		return nil, err
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	originalDirector := proxy.Director
+	proxy.Director = func(req *http.Request) {
+		originalDirector(req)
+		modifyRequest(req)
+	}
+
+	proxy.ModifyResponse = modifyResponse()
+	proxy.ErrorHandler = errorHandler()
+	return proxy, nil
+}
+
+func modifyRequest(req *http.Request) {
+	req.Header.Set("X-Proxy", "Simple-Reverse-Proxy")
+}
+
+func errorHandler() func(http.ResponseWriter, *http.Request, error) {
+	return func(w http.ResponseWriter, req *http.Request, err error) {
+		fmt.Printf("Got error while modifying response: %v \n", err)
+	}
+}
+
+func modifyResponse() func(*http.Response) error {
+	return func(resp *http.Response) error {
+		return errors.New("response body is invalid")
+	}
+}
+
+// ProxyRequestHandler handles the http request using proxy
+func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	}
+}
+
+var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+func shortID(length int) string {
+	ll := len(chars)
+	b := make([]byte, length)
+	rand.Read(b) // generates len(b) random bytes
+	for i := 0; i < length; i++ {
+		b[i] = chars[int(b[i])%ll]
+	}
+	return string(b)
+}
